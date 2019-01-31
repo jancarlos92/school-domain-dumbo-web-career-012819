2abee8eb34653cd5bf2054ae748fb3baf46de0b1
Jancarlos Acevedo <Jancarlos92@me.com>
2019-01-31 00:40:15 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+
+require 'pry'
+require 'rb-readline'
+
+
+class School
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+    if @roster.include?(grade)
+    @roster[grade] << name
+    else @roster[grade] = []
+    @roster[grade] << name
+    end
+    end
+
+    def grade(grade_number)
+      @roster[grade_number]
+    end
+
+    def sort
+      @roster.each_value { |v| v.sort! }
+
+
+      # sorted = @roster.select
+      # return sorted
+      end
+end
